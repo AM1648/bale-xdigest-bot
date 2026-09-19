5050380b8eb5179223a6972787bc4ba4789900d8
mosallanezhad <mosallanezhad@inside.sahab.ir>
2026-09-19 09:26:59 +0330

    Fix bug of not showing full text for retweets

--- core/tweet_renderer.py
+++ core/tweet_renderer.py
@@ -7,7 +7,6 @@ from core.persian_text import PersianText
 class TweetRenderer:
     # Bale always parses Markdown: escape these wherever they appear as literal text
     _MARKDOWN = str.maketrans({c: "\\" + c for c in "*`_"})
-    _RT_PREFIX = re.compile(r"^RT @\w+:\s*")
 
     def __init__(self, persian: PersianText):
         self.persian = persian
@@ -18,12 +17,11 @@ class TweetRenderer:
 
         if tweet.has_retweet and tweet.original_link:
             header = f"✍️ [{_esc(tweet.original_screen_name)}]({tweet.original_link}) (🔃 {_esc(user.user_name)})"
+            body_raw = tweet.long_text if tweet.long_text is not None else tweet.original_text
         else:
             header = f"✍️ [{_esc(user.user_name)}]({tweet.link})"
+            body_raw = tweet.long_text if tweet.long_text is not None else tweet.text
 
-        # Body (strip leading "RT @username:")
-        body_raw = tweet.long_text if tweet.long_text is not None else tweet.text
-        body_raw = self._RT_PREFIX.sub("", body_raw or "")
         body = _esc(body_raw)
 
         quote = ""
